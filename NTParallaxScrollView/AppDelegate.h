44f62cd8d98e9285ab03a6f3d52aeb2a7e786a42
Nicholas Tau <demon42111915@gmail.com>
2013-07-07 11:33:47 +0800

    Initial Commit

---
+++ NTParallaxScrollView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NTParallaxScrollView
+//
+//  Created by demon on 7/7/13.
+//  Copyright (c) 2013 demon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NTParallaxScrollView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NTParallaxScrollView
+//
+//  Created by demon on 7/7/13.
+//  Copyright (c) 2013 demon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
